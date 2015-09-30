f9ab06b79a3e49a16dfe2ecc315cf0ed14061e98
Marc Liu <liusenmiao@gmail.com>
2015-09-29 22:42:48 -0700

    Add API skeletons for `Date`

--- runtime/js/core/03date.js
+++ runtime/js/core/03date.js
@@ -2,6 +2,309 @@
 // Licensed under the Apache License v2.0. See LICENSE in the project
 // root for complete license information.
 
-Date = function Date (year, month, date, hours, minutes, seconds, ms)
+/* global hidden */
+
+Date = function Date(year, month, date, hours, minutes, seconds, ms)
 {
+
 };
+
+hidden(Date, "UTC", function date_utc(x)
+{
+
+});
+
+
+hidden(Date, "parse", function date_parse(x)
+{
+
+});
+
+
+hidden(Date, "now", function date_now(x)
+{
+
+});
+
+
+
+hidden(Date, "constructor", function date_constructor(x)
+{
+
+});
+
+
+hidden(Date, "toString", function date_tostring(x)
+{
+
+});
+
+
+hidden(Date, "toDateString", function date_todatestring(x)
+{
+
+});
+
+
+hidden(Date, "toTimeString", function date_totimestring(x)
+{
+
+});
+
+
+hidden(Date, "toLocaleString", function date_tolocalestring(x)
+{
+
+});
+
+
+hidden(Date, "toLocaleDateString", function date_tolocaledatestring(x)
+{
+
+});
+
+
+hidden(Date, "toLocaleTimeString", function date_tolocaletimestring(x)
+{
+
+});
+
+
+hidden(Date, "valueOf", function date_valueof(x)
+{
+
+});
+
+
+hidden(Date, "getTime", function date_gettime(x)
+{
+
+});
+
+
+hidden(Date, "getFullYear", function date_getfullyear(x)
+{
+
+});
+
+
+hidden(Date, "getUTCFullYear", function date_getutcfullyear(x)
+{
+
+});
+
+
+hidden(Date, "getMonth", function date_getmonth(x)
+{
+
+});
+
+
+hidden(Date, "getUTCMonth", function date_getutcmonth(x)
+{
+
+});
+
+
+hidden(Date, "getDate", function date_getdate(x)
+{
+
+});
+
+
+hidden(Date, "getUTCDate", function date_getutcdate(x)
+{
+
+});
+
+
+hidden(Date, "getDay", function date_getday(x)
+{
+
+});
+
+
+hidden(Date, "getUTCDay", function date_getutcday(x)
+{
+
+});
+
+
+hidden(Date, "getHours", function date_gethours(x)
+{
+
+});
+
+
+hidden(Date, "getUTCHours", function date_getutchours(x)
+{
+
+});
+
+
+hidden(Date, "getMinutes", function date_getminutes(x)
+{
+
+});
+
+
+hidden(Date, "getUTCMinutes", function date_getutcminutes(x)
+{
+
+});
+
+
+hidden(Date, "getSeconds", function date_getseconds(x)
+{
+
+});
+
+
+hidden(Date, "getUTCSeconds", function date_getutcseconds(x)
+{
+
+});
+
+
+hidden(Date, "getMilliseconds", function date_getmilliseconds(x)
+{
+
+});
+
+
+hidden(Date, "getUTCMilliseconds", function date_getutcmilliseconds(x)
+{
+
+});
+
+
+hidden(Date, "getTimezoneOffset", function date_gettimezoneoffset(x)
+{
+
+});
+
+
+hidden(Date, "setTime", function date_settime(x)
+{
+
+});
+
+
+hidden(Date, "setMilliseconds", function date_setmilliseconds(x)
+{
+
+});
+
+
+hidden(Date, "setUTCMilliseconds", function date_setutcmilliseconds(x)
+{
+
+});
+
+
+hidden(Date, "setSeconds", function date_setseconds(x)
+{
+
+});
+
+
+hidden(Date, "setUTCSeconds", function date_setutcseconds(x)
+{
+
+});
+
+
+hidden(Date, "setMinutes", function date_setminutes(x)
+{
+
+});
+
+
+hidden(Date, "setUTCMinutes", function date_setutcminutes(x)
+{
+
+});
+
+
+hidden(Date, "setHours", function date_sethours(x)
+{
+
+});
+
+
+hidden(Date, "setUTCHours", function date_setutchours(x)
+{
+
+});
+
+
+hidden(Date, "setDate", function date_setdate(x)
+{
+
+});
+
+
+hidden(Date, "setUTCDate", function date_setutcdate(x)
+{
+
+});
+
+
+hidden(Date, "setMonth", function date_setmonth(x)
+{
+
+});
+
+
+hidden(Date, "setUTCMonth", function date_setutcmonth(x)
+{
+
+});
+
+
+hidden(Date, "setFullYear", function date_setfullyear(x)
+{
+
+});
+
+
+hidden(Date, "setUTCFullYear", function date_setutcfullyear(x)
+{
+
+});
+
+
+hidden(Date, "toGMTString", function date_togmtstring(x)
+{
+
+});
+
+
+hidden(Date, "toUTCString", function date_toutcstring(x)
+{
+
+});
+
+
+hidden(Date, "getYear", function date_getyear(x)
+{
+
+});
+
+
+hidden(Date, "setYear", function date_setyear(x)
+{
+
+});
+
+
+hidden(Date, "toISOString", function date_toisostring(x)
+{
+
+});
+
+
+hidden(Date, "toJSON", function date_tojson(x)
+{
+
+});
