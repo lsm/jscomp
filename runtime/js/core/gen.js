ff47ce416e61aac25c43c8534540baf51ebc6f3d
Marc Liu <liusenmiao@gmail.com>
2015-09-29 22:43:05 -0700

    API skeleton generator.

---
+++ runtime/js/core/gen.js
@@ -0,0 +1,34 @@
+var util = require("util")
+var name = process.argv[2]
+
+console.error("Generate API skeletons for %s", name);
+
+
+function printCode(name, childName)
+{
+    var fnName = [name.toLowerCase(), childName.toLowerCase()].join("_")
+    var tpl = "hidden(%s, \"%s\", function %s(x)\n{\n\n});\n\n"
+    var code = util.format(tpl, name, childName, fnName)
+    console.log(code);
+}
+
+function loop(obj, cb)
+{
+    obj && Object.getOwnPropertyNames(obj).forEach(function(k)
+    {
+        if (typeof obj[k] === "function")
+        {
+            cb(k)
+        }
+    })
+}
+
+loop(global[name], function(childName)
+{
+    printCode(name, childName)
+})
+
+loop(global[name].prototype, function(childName)
+{
+    printCode(name, childName)
+})
